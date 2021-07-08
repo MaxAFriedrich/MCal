7455b0d603da50f35811a387a9749ed6df49690f
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2021-07-08 12:37:57 +0100

    got tables working!

--- src/note/note.ts
+++ src/note/note.ts
@@ -1,9 +1,8 @@
-import { setContEdit } from '../gui/guiElement'
+import {setContEdit} from "../gui/guiElement";
 
 //*global vars
 //toggle for preview
-var previewMDState:boolean=true;
-
+var previewMDState: boolean = true;
 
 /**
  * function that runs on button click which converts to makrkdown to html and back on toggle
@@ -12,92 +11,84 @@ var previewMDState:boolean=true;
  * @param currentWrite the current contents of the notepad file
  * @returns [text to write to file, text to display] strings
  */
-export function previewMDClick(currentDisplayText:string,currentDisplayHTML:string,currentWrite:string){
-  var toWrite:string, toDisplay:string, value:string;
+export function previewMDClick(currentDisplayText : string, currentDisplayHTML : string, currentWrite : string) {
+  let toWrite: string,
+    toDisplay: string,
+    value: string;
 
-  if (previewMDState){
+  if (previewMDState) {
     // show html
-    setContEdit(false,"note");
-    toDisplay=markdownParser(currentDisplayText);
-    toWrite=currentDisplayHTML;
-    value="Edit";
-  }else{
+    setContEdit(false, "note");
+    toDisplay = markdownParser(currentDisplayText);
+    toWrite = currentDisplayHTML;
+    value = "Edit";
+  } else {
     //show code
-    setContEdit(true,"note");
-    toDisplay=currentWrite;
-    toWrite=currentWrite;
-    value="Preview";
+    setContEdit(true, "note");
+    toDisplay = currentWrite;
+    toWrite = currentWrite;
+    value = "Preview";
   }
-  previewMDState=!previewMDState
-  return [toWrite,toDisplay,value]
+  previewMDState = !previewMDState;
+  return [toWrite, toDisplay, value];
 }
 
-//!NOTE: This is not completed code and has been pulled directily from https://github.com/MaxAFriedrich/markdown-parser
-//TODO tables and footnotes
+//*NOTE: This is not completed code and has been pulled mostly from https://github.com/MaxAFriedrich/markdown-parser
+//TODO footnotes
 function markdownParser(input : string) {
-  var output:string= input.replace(/^###### (.*)\{#(.*)\}$/gim, '<h6 id="$2">$1</h6>')
-  .replace(/^###### (.*$)/gim, "<h6>$1</h6>")
-  .replace(/^##### (.*)\{#(.*)\}$/gim, '<h5 id="$2">$1</h5>')
-  .replace(/^##### (.*$)/gim, "<h5>$1</h5>")
-  .replace(/^#### (.*)\{#(.*)\}$/gim, '<h4 id="$2">$1</h4>')
-  .replace(/^#### (.*$)/gim, "<h4>$1</h4>")
-  .replace(/^### (.*)\{#(.*)\}$/gim, '<h3 id="$2">$1</h3>')
-  .replace(/^### (.*$)/gim, "<h3>$1</h3>")
-  .replace(/^## (.*)\{#(.*)\}$/gim, '<h2 id="$2">$1</h2>')
-  .replace(/^## (.*$)/gim, "<h2>$1</h2>")
-  .replace(/^# (.*)\{#(.*)\}$/gim, '<h1 id="$2">$1</h1>')
-  .replace(/^# (.*$)/gim, "<h1>$1</h1>")
-  .replace(/^---/gim, "<hr>")
-  .replace(/^___/gim, "<hr>")
-  .replace(/^\*\*\*/gim, "<hr>")
-  .replace(/^\> (.*$)/gim, "<blockquote>$1</blockquote>")
-  .replace(/\*\*\*(.*)\*\*\*/gim, "<i><b>$1</b></i>")
-  .replace(/\*\*(.*)\*\*/gim, "<b>$1</b>")
-  .replace(/__(.*)__/gim, "<b>$1</b>")
-  .replace(/\*(.*)\*/gim, "<i>$1</i>")
-  .replace(/_(.*)_/gim, "<i>$1</i>")
-  .replace(/~~(.*)~~/gim, "<s>$1</s>")
-  .replace(/^- \[\x\](\s*)(.*)/gim, '<ul style="list-style-type: none;"><li><input type="checkbox"checked="true">$2</li></ul>')
-  .replace(/^- \[ \](\s*)(.*)/gim, '<ul style="list-style-type: none;"><li><input type="checkbox">$2</li></ul>')
-  .replace(/^(\*|\-|\+ )(\s*)(.*)/gim, "<ul><li>$3</li></ul>")
-  .replace(/(^1\.\s*(.|\n)*\d\.\s*.*)/gim, "<ol>$1</ol>")
-  .replace(/\d\.\s*(.*)/gim, "<li>$1</li>")
-  .replace(/\=\=(.*)\=\=/gim, "<mark>$1</mark>")
-  .replace(/\`\`\`(.*)([\s\S]*)\`\`\`/gim, "<pre>$2</pre>")
-  .replace(/^(.*)\n: (.*)/gim, "<p><dfn>$1</dfn> $2</p>")
-  .replace(/\`(.*)\`/gim, "<code>$1</code>")
-  .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
-  .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="javascript:void(0);" onclick="const { shell } = require(`electron`);shell.openExternal(`$2`);">$1</a>')
-  .replace(/\n$/gim, "<br />");
-  console.log(output);
-  return markdownTable(output)
+  let output: string = input.replace(/^###### (.*)\{#(.*)\}$/gim, '<h6 id="$2">$1</h6>').replace(/^###### (.*$)/gim, "<h6>$1</h6>").replace(/^##### (.*)\{#(.*)\}$/gim, '<h5 id="$2">$1</h5>').replace(/^##### (.*$)/gim, "<h5>$1</h5>").replace(/^#### (.*)\{#(.*)\}$/gim, '<h4 id="$2">$1</h4>').replace(/^#### (.*$)/gim, "<h4>$1</h4>").replace(/^### (.*)\{#(.*)\}$/gim, '<h3 id="$2">$1</h3>').replace(/^### (.*$)/gim, "<h3>$1</h3>").replace(/^## (.*)\{#(.*)\}$/gim, '<h2 id="$2">$1</h2>').replace(/^## (.*$)/gim, "<h2>$1</h2>").replace(/^# (.*)\{#(.*)\}$/gim, '<h1 id="$2">$1</h1>').replace(/^# (.*$)/gim, "<h1>$1</h1>").replace(/^---/gim, "<hr>").replace(/^___/gim, "<hr>").replace(/^\*\*\*/gim, "<hr>").replace(/^\> (.*$)/gim, "<blockquote>$1</blockquote>").replace(/\*\*\*(.*)\*\*\*/gim, "<i><b>$1</b></i>").replace(/\*\*(.*)\*\*/gim, "<b>$1</b>").replace(/__(.*)__/gim, "<b>$1</b>").replace(/\*(.*)\*/gim, "<i>$1</i>").replace(/_(.*)_/gim, "<i>$1</i>").replace(/~~(.*)~~/gim, "<s>$1</s>").replace(/^- \[\x\](\s*)(.*)/gim, '<ul style="list-style-type: none;"><li><input type="checkbox"checked="true">$2</li></ul>').replace(/^- \[ \](\s*)(.*)/gim, '<ul style="list-style-type: none;"><li><input type="checkbox">$2</li></ul>').replace(/^(\*|\-|\+ )(\s*)(.*)/gim, "<ul><li>$3</li></ul>").replace(/(^1\.\s*(.|\n)*\d\.\s*.*)/gim, "<ol>$1</ol>").replace(/\d\.\s*(.*)/gim, "<li>$1</li>").replace(/\=\=(.*)\=\=/gim, "<mark>$1</mark>").replace(/\`\`\`(.*)([\s\S]*)\`\`\`/gim, "<pre>$2</pre>").replace(/^(.*)\n: (.*)/gim, "<p><dfn>$1</dfn> $2</p>").replace(/\`(.*)\`/gim, "<code>$1</code>").replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />").replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="javascript:void(0);" onclick="const { shell } = require(`electron`);shell.openExternal(`$2`);">$1</a>').replace(/\n$/gim, "<br />");
+  return markdownTable(output);
 }
 
-function markdownTable(inputHTML:string){
-  var outputHTML:string[]=inputHTML.split("<br />")
-  // for (var i = 0;i<outputHTML.length;i++){
-  //   if (i + 1 != outputHTML.length && outputHTML[i].startsWith("| ") && outputHTML[i].endsWith(" |") && outputHTML[i].includes(" | ") && outputHTML[i + 1].includes("| -") && outputHTML[i + 1].includes("---")) {
-  //     var table = "";
-  //     var j;
-  //     for (j = i; j < outputHTML.length; j++) {
+/**
+ * get html and check fo markdown tables and return with formated html tables
+ * @param inputHTML html string
+ * @returns html string
+ */
+function markdownTable(inputHTML : string) {
+  let outputHTML: string[] = inputHTML.split("<br />");
+  for (let i = 0; i < outputHTML.length; i++) {
+    let line: string = outputHTML[i];
+    if (line.includes(" | ")) {
+      //get the potential table
+      //lineArray=entire table inside html line
+      let lineArray: string[] = line.split("\n");
+      lineArray.splice(0, 1);
 
-  //         if (outputHTML[j].startsWith("| ") && outputHTML[j].endsWith(" |") && outputHTML[j].includes(" | ") && outputHTML[j].includes("---") == false) {
-  //             table = "".concat(table, outputHTML[j]);
-  //             outputHTML[j] = "";
-  //         } else if (outputHTML[j].includes("---")) {
-  //             outputHTML[j] = "";
-  //         }
-  //     }
-  //     table = table.split(" | ").join("</td><td>");
+      //is it a table?
+      if (lineArray.length > 3) {
+        //yes is table
+        for (let j = 0; j < lineArray.length; j++) {
+          if (j < lineArray.length - 1 && lineArray[j + 1].includes("| ---")) {
+            //heading row
+            let tempLine: string[] = lineArray[j].split("|");
+            tempLine.splice(0, 1);
+            tempLine.splice(tempLine.length - 1, 1);
+            let outLine: string = "<tr>";
+            tempLine.forEach((cell) => {
+              outLine += "<th>" + cell.trim() + "</th>";
+            });
+            lineArray[j] = outLine + "</tr>";
 
-  //     table = table.split(" |").join("</td></tr>");
+            //remove next line
+            lineArray.splice(j + 1, 1);
+          } else {
+            //body row
+            let tempLine: string[] = lineArray[j].split("|");
+            tempLine.splice(0, 1);
+            tempLine.splice(tempLine.length - 1, 1);
+            let outLine: string = "<tr>";
+            tempLine.forEach((cell) => {
+              outLine += "<td>" + cell.trim() + "</td>";
+            });
+            lineArray[j] = outLine + "</tr>";
+          }
+        }
 
-  //     table = table.split("| ").join("<tr><td>");
-  //     console.log(table);
-
-  //     outputHTML[i] = "".concat("<table>", table, "</table>");
-  //   }
-  // }
-  console.log(outputHTML)
-  return outputHTML.join("<br />")
+        //write lineArray back to outputHTML[i]
+        outputHTML[i] = "<table>" + lineArray.join("") + "</table>";
+      }
+    }
+  }
+  return outputHTML.join("<br />");
 }
