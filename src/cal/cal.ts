4234213bcfa661bca141c8c83e7f2d30ae123fc9
Hugo Whittome <hugowhittome@gmail.com>
2021-07-09 10:35:35 +0100

    Commented all the functions

--- src/cal/cal.ts
+++ src/cal/cal.ts
@@ -2,29 +2,46 @@ import { ClassName, scrollToFirstElementWithClassNames } from "../gui/className"
 import * as Selector from "./dateSelector"
 import * as DayWrapper from "./dayWrapper/dayWrapper"
 
+/**
+ * Initialises each section and displays the starting section
+ * @param fromFile contents of .json save file
+ */
 export function init(fromFile: string) {
   DayWrapper.init(fromFile);
   Selector.init(dateDisplay);
   dateDisplay();
-
 }
 
+/**
+ * Function to be called when the events have been edited
+ */
 export function eventChanged() {
   console.log("Calendar events have changed, updating CalDay");
   DayWrapper.extractFromHTML();
 }
 
+/**
+ * Function to be called when the selected day in the text box has been changed
+ */
 export function selectedDayChanged() {
   if (Selector.hasDateSelectorChanged()) {
     dateDisplay();
   }
 }
 
+/**
+ *
+ * @returns string to save to a json file
+ */
 export function getSaveFileString(): string {
   return DayWrapper.getFileSaveString(); // TODO: Add selected day to save?
 }
 
 //* Private
+
+/**
+ * Displays the events for the selected day
+ */
 function dateDisplay() {
   var selectedDay = Selector.getSelectedDay();
   Selector.displaySelector();
--- src/cal/dayWrapper/calEvent.ts
+++ src/cal/dayWrapper/calEvent.ts
@@ -3,23 +3,6 @@ import { ClassName, ElementID, createDiv, createSpan } from "../../gui/creation"
 
 // const DEFAULT_COLOUR: Color = Color.rgb(0, 0, 0);
 const DEFAULT_COLOUR: string = "#FFFFFF"
-
-export function createEventBar(isSelected: boolean = false, onDivClick: () => void = null, startTime: string = "", endTime: string = "", description: string = "") {
-	var classNames = [ClassName.event].concat((isSelected ? [ClassName.selected] : []))
-	var myDiv: HTMLDivElement;
-	if (onDivClick != null) {
-		myDiv = createDiv(classNames, onDivClick);
-
-	} else {
-		myDiv = createDiv(classNames);
-	}
-	myDiv.appendChild(createSpan(ElementID.eventStartTime, "12:00PM", "true", false, startTime));
-	myDiv.appendChild(createSpan(ElementID.eventEndTime, "1:00PM", "true", false, endTime));
-	myDiv.appendChild(createSpan(ElementID.eventContents, "Event Description", "true", false, description));
-
-	return myDiv;
-}
-
 export class CalEvent {
 	private description: string;
 	private startTime: string;
@@ -35,29 +18,86 @@ export class CalEvent {
 		this.notes = notes;
 	}
 
+	//* Static
+	/**
+	 * Creates CalEvent from the given json
+	 * @param json string to be parsed
+	 * @returns CalEvent
+	 */
 	public static getEventFromJSON(json: string): CalEvent {
 		var obj = JSON.parse(json);
 
 		return new CalEvent(obj["description"], obj["startTime"], obj["endTime"], obj["colour"], obj["notes"]);
 	}
 
-	public getDiv(isSelected: boolean, onDivClick: () => void): HTMLDivElement {
-		return createEventBar(isSelected, onDivClick, this.startTime, this.endTime, this.description);
+	/**
+	 *
+	 * @param isSelected whether it should be selected
+	 * @param onDivClick function to attach to the div
+	 * @param startTime
+	 * @param endTime
+	 * @param description
+	 * @param colour of the div
+	 * @param notes
+	 * @returns div for the event (excluding delete button)
+	 */
+  public static createBar(isSelected: boolean = false, onDivClick: () => void = null, startTime: string = "", endTime: string = "", description: string = "", colour: string = DEFAULT_COLOUR, notes: string = ""): HTMLDivElement {
+		// TODO: Selected should show notes panel and should change colour depending on the parameter
+		var classNames = [ClassName.event].concat((isSelected ? [ClassName.selected] : []))
+		var myDiv: HTMLDivElement;
+		if (onDivClick != null) {
+			myDiv = createDiv(classNames, onDivClick);
+
+		} else {
+			myDiv = createDiv(classNames);
+		}
+		myDiv.appendChild(createSpan(ElementID.eventStartTime, "12:00PM", "true", false, startTime));
+		myDiv.appendChild(createSpan(ElementID.eventEndTime, "1:00PM", "true", false, endTime));
+		myDiv.appendChild(createSpan(ElementID.eventContents, "Event Description", "true", false, description));
+
+		return myDiv;
 	}
 
+	//* Getters and setters
+	/**
+	 *
+	 * @param isSelected whether the event should be selected
+	 * @param onDivClick function to be called when the div is clicked
+	 * @returns Div for this event
+	 */
+	public getDiv(isSelected: boolean, onDivClick: () => void): HTMLDivElement {
+		return CalEvent.createBar(isSelected, onDivClick, this.startTime, this.endTime, this.description, this.colour, this.notes);
+	}
+
+	/**
+	 *
+	 * @returns The start time of the event
+	 */
+	public getStartTime(): string {
+		return this.startTime;
+	}
+
+	/**
+	 *
+	 * @param desc to set
+	 */
 	public setDescription(desc: string): void {
 		this.description = desc;
 	}
 
+	/**
+	 *
+	 * @param time to set to event start time
+	 */
 	public setStartTime(time: string): void {
 		this.startTime = time;
 	}
 
+	/**
+	 *
+	 * @param time to set to event end time
+	 */
 	public setEndTime(time: string): void {
 		this.description = time;
-	}
-
-	public getStartTime(): string {
-		return this.startTime;
 	}
 }
